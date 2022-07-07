1ca240464d51b73bc5905311050a54dd6bd5c44a
Akumar132232 <64557746+Akumar132232@users.noreply.github.com>
2022-07-07 14:55:45 +0530

    uc2_PlayerChooseXorO
    uc2_PlayerChooseXorO

---
+++ uc2_tictac.java
@@ -0,0 +1,32 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class uc2_tictac {
+
+    private static char[] element;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tic Tac Toe Game....!!!");
+        board();
+        userinput();
+    }
+    public static void board() {
+        element = new char[10];
+        for(int i=1; i<10; i++) {
+            element[i]=' ';
+        }
+    }
+    public static void userinput() {
+        Scanner in=new Scanner(System.in);
+        System.out.println(" Select Your Marker (O or X): ");
+        char player=in.next().charAt(0);
+        char computer;
+        if (player=='X') {
+            computer='O';
+            System.out.println("User mark is :" +player);}
+
+        else
+            computer='X';
+        System.out.println("User mark is :" +player);
+    }
+}
