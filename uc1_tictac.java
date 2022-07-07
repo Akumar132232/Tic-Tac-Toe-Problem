2b948868458fc5996774eecd6e48d978f5bcd3ca
Akumar132232 <64557746+Akumar132232@users.noreply.github.com>
2022-07-07 14:53:55 +0530

    uc1_tictactoe
    uc1_tictactoe

---
+++ uc1_tictac.java
@@ -0,0 +1,19 @@
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class uc1_tictac {
+
+    private static char[] element;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tic Tac Toe Game....!!!");
+        board();
+    }
+    private static void board() {
+        element = new char[10];
+        for(int i=1; i<10; i++) {
+            element[i]='.';
+        }
+    }
+}
